3ffc8ab7986f3d22b0bab83e338189875f5d03d2
Billy Hardy <hardywb@leidos.com>
2016-11-02 22:42:07 -0400

    added beans and test to test hash collisions

--- src/test/java/ds/util/HashSetTest.java
+++ src/test/java/ds/util/HashSetTest.java
@@ -5,6 +5,9 @@ import org.junit.Test;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 
+import ds.util.beans.Person;
+import ds.util.beans.PersonBadHash;
+
 public class HashSetTest {
 
     public HashSetTest() {}
@@ -23,6 +26,19 @@ public class HashSetTest {
 	    boolean addedSecond = set.add(5);
 	    assertTrue(added);
 	    assertFalse(addedSecond);
+	}
+
+    @Test
+	public void testHashSet_AddTwiceCollision() {
+	    HashSet<PersonBadHash> set = new HashSet<>();
+	    boolean addedFirst = set.add(new PersonBadHash(5));
+	    boolean addedSecond = set.add(new PersonBadHash(15));
+	    boolean containsFirst = set.contains(new PersonBadHash(5));
+	    boolean containsSecond = set.contains(new PersonBadHash(15));
+	    assertTrue(addedFirst);
+	    assertTrue(containsFirst);
+	    assertTrue(addedSecond);
+	    assertTrue(containsSecond);
 	}
 }
 
