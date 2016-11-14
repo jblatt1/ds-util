9de1d877be4fe4227b91bc5ec374ea8f6e517861
Billy Hardy <billy92@gmail.com>
2016-11-13 22:01:56 -0500

    added add, get and grow to ArrayList

--- src/main/java/ds/util/ArrayList.java
+++ src/main/java/ds/util/ArrayList.java
@@ -4,14 +4,40 @@ package ds.util;
 public class ArrayList<T> {
     private Object[] items;
     private int size;
+    private int capacity;
 
     public ArrayList() {
 	this(10);
     }
 
-    public ArrayList(int size) {
-	this.size = size;
-	this.items = new Object[this.size]; 
+    private ArrayList(int capacity) {
+	this.size = 0;
+	this.grow(capacity); 
+    }
+
+    private void grow(int capacity) {
+	this.capacity = capacity;
+	Object[] items = this.items;
+	this.items = new Object[this.capacity];
+	for(int i=0; i<this.size; i++) {
+	    this.add(items[i]);
+	}
+    }
+
+
+    public boolean add(Object t) {
+	if(this.capacity <= this.size) {
+	    this.grow(this.capacity * 2);
+	}
+	this.items[this.size++] = t;
+	return true;
+    }
+
+    public T get(int i) throws IndexOutOfBoundsException {
+	if(i >= this.size) {
+	    throw new IndexOutOfBoundsException();
+	}
+	return (T) this.items[i];
     }
 
 }
